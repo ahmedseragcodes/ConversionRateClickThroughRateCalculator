6de4c235f985011012aa2f179858bf5c8d79f89a
ahmedseragcodes <ahmedserag1@gmail.com>
2021-09-01 20:43:28 -0400

    reformatting for vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Styles from "./styles.scss";
+import "./styles.scss";
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById("root");
